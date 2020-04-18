00bc3ea0a0605a8972e37a7b21da9841db1b6b0c
HieuDev <hieudevsoft@gmail.com>
2020-04-18 09:17:42 +0700

    add dowload img

--- app/src/main/java/com/example/demonhe/MainActivity.kt
+++ app/src/main/java/com/example/demonhe/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.demonhe
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.bell.view.*
+import java.net.HttpURLConnection
+import java.net.URI
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 
 class MainActivity : AppCompatActivity()
 {
@@ -18,6 +25,30 @@ class MainActivity : AppCompatActivity()
 
         bt_Increase.setOnClickListener {
             SetCountNotifications().increaseNotifications(layout_bell)
+        }
+        button.setOnClickListener {
+            DownLoadImage()
+                .execute("https://nnimgt-a.akamaihd.net/transform/v1/crop/frm/silverstone-feed-data/dfc46751-bc35-4c59-ac94-e4861e4b7eae.jpg/r0_0_800_600_w1200_h678_fmax.jpg")
+        }
+    }
+    inner class DownLoadImage: AsyncTask<String, Void, Bitmap>() {
+        override fun doInBackground(vararg params: String?): Bitmap {
+            var bitmap:Bitmap? = null
+            val url:URL = URL(params[0]!!)
+            val http = url.openConnection() as HttpsURLConnection
+            http.connectTimeout = 5000
+            http.readTimeout = 10000
+            http.requestMethod = "GET"
+            http.connect()
+
+            val inputStream = http.inputStream
+            bitmap = BitmapFactory.decodeStream(inputStream)
+            return bitmap!!
+        }
+
+        override fun onPostExecute(result: Bitmap?) {
+            Toast.makeText(applicationContext,"Done",Toast.LENGTH_SHORT).show()
+            imageView.setImageBitmap(result)
         }
 
     }
